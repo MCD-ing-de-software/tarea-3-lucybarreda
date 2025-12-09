8eda46f425c74a48fffb37c9c85038bfbf2c6c19
lucybarreda <146671084+lucybarreda@users.noreply.github.com>
2025-12-09 17:53:22 -0500

    A1 Test3

--- tests/test_statistics_utils.py
+++ tests/test_statistics_utils.py
@@ -91,6 +91,12 @@ class TestStatisticsUtils(unittest.TestCase):
         - Crear una secuencia bidimensional (ej: [[1, 2], [3, 4]])
         - Llamar a moving_average con esa secuencia y verificar que se lanza un ValueError indicando que solo se aceptan secuencias 1D (usar self.assertRaises)
         """
+         utils = StatisticsUtils()
+         arr_2d = np.array([[1, 2], [3, 4]])
+         with self.assertRaises(ValueError) as cm:
+            utils.moving_average(arr_2d, window=2)
+        self.assertIn("Only 1D sequences are accepted.", str(cm.exception))
+
 
     def test_zscore_has_mean_zero_and_unit_std(self):
         """Test que verifica que el método zscore calcula correctamente los z-scores
